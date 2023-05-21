8e2e92d6e35621ff5e6dbe9a720b3d43eb559150
alkher27 <alikheraj@gmail.com>
2023-05-21 22:59:35 +0000

    2nd

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,24 @@
 class PlacesController < ApplicationController
   def index
     #find all places
-    @places = Places.all
+    @places = Place.all
   end
+  
+  def create
+    #   # start with a new Company
+      @places = Place.new
+    #   # assign user-entered form data to Company's columns
+      @places["name"] = params["places"]["name"]
+    #   # save Company row
+      @places.save
+  
+    #   # redirect user
+    redirect_to "/places"
+    end
+  
+    def new
+      @place = Place.new
+      #render view with new Company form  
+    end
+
 end
